17f14cf50b6160119bdca26d7472bd8ac058689e
Clement Bourgoin <cb@nokto.net>
2017-12-17 21:15:48 +0100

    Add a Person component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react App</h1>
+        <Person name="Clément" age="33"/>
+        <Person name="Mathilde" age="29">Hobbies: pottery</Person>
       </div>
     );
   }
